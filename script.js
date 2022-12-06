6e9536d7a86bcf8912c3b782c3ed173f10fd91e8
Mazen Abukhalaf <94471446+MazenAbukhalaf@users.noreply.github.com>
2022-12-05 22:09:20 -0500

    1.0
    First stable release of DSIS (Dual Scoreboard Interface System)

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
+function autoUpdate() {
+    setInterval(function() { ObserveInputValue($('#showname').val()); }, 100);
+}
 function submitForm() {
     var teamNames = new Array(document.getElementById("teamonename").value, document.getElementById("teamtwoname").value, document.getElementById("teamthreename").value, document.getElementById("teamfourname").value, document.getElementById("teamfivename").value, document.getElementById("teamsixname").value);
     var teamScores = new Array(document.getElementById("teamonescore").value, document.getElementById("teamtwoscore").value, document.getElementById("teamthreescore").value, document.getElementById("teamfourscore").value, document.getElementById("teamfivescore").value, document.getElementById("teamsixscore").value);
-    var teamColors = new Array(document.getElementById("teamonecolor").value, document.getElementById("teamtwocolor").value, document.getElementById("teamthreecolor").value, document.getElementById("teamfourcolor").value, document.getElementById("teamfivecolor").value, document.getElementById("teamsixcolor").value);
-    var showBKG = document.getElementById("nobkg").value;
+    var colorSelector = document.getElementById("colsel").value;
+    var colorSelector2 = document.getElementById("colsel2").value;
+    // var fontSize = document.getElementById("fontsize").value;
+    // var titleSize = document.getElementById("titlesize").value;
+    if (document.getElementById('nobkg').checked) {
+        var showBKG = "on";
+    } else {
+        var showBKG = "off";
+    }
+    if (document.getElementById('notxt').checked) {
+        var showTXT = "on";
+    } else {
+        var showTXT = "off";
+    }
     var showName = document.getElementById("showname").value;
     console.log(teamNames);
     localStorage.setItem('teamNamesLocal', JSON.stringify(teamNames));
     console.log(teamScores);
     localStorage.setItem('teamScoresLocal', JSON.stringify(teamScores));
-    console.log(teamColors);
-    localStorage.setItem('teamColorsLocal', JSON.stringify(teamColors));
     console.log(showName);
     localStorage.setItem('showNameLocal', JSON.stringify(showName));
     console.log(showBKG);
     localStorage.setItem('showBackgroundLocal', JSON.stringify(showBKG));
+    console.log(showTXT);
+    localStorage.setItem('showTextLocal', JSON.stringify(showTXT));
+    // localStorage.setItem('fontSizeLocal', fontSize + 'px');
+    // console.log(localStorage.getItem("fontSizeLocal"));
+    // localStorage.setItem('titleSizeLocal', titleSize + 'px');
+    // console.log(localStorage.getItem("titleSizeLocal"));
+    console.log(colorSelector);
+    localStorage.setItem('colorSelectorLocal', JSON.stringify(colorSelector));
+    console.log(colorSelector2);
+    localStorage.setItem('colorSelector2Local', JSON.stringify(colorSelector2));
+    document.getElementById("updated").style.display = "block";
     //scoreform.submit(); // Submit the form
     return false; // Prevent page refresh
+}
+
+function enableAdvanced() {
+    var x = document.getElementById("advancedoptions");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
--- script2.js
+++ script2.js
@@ -1,3 +1,4 @@
+const addCSS = css => document.head.appendChild(document.createElement("style")).innerHTML=css;
 function submitForm() {
 }
 setInterval(function(){
@@ -5,8 +6,12 @@ setInterval(function(){
     document.getElementById('showname').textContent = storedShowName;
     var storedTeamNames = JSON.parse(localStorage.getItem('teamNamesLocal'));
     var storedTeamScores = JSON.parse(localStorage.getItem('teamScoresLocal'));
-    var storedTeamColors = JSON.parse(localStorage.getItem('teamColorsLocal'));
     var storedShowBKG = JSON.parse(localStorage.getItem('showBackgroundLocal'));
+    var storedShowTXT = JSON.parse(localStorage.getItem('showTextLocal'));
+    var storedColorSelector = JSON.parse(localStorage.getItem('colorSelectorLocal'));
+    var storedColorSelector2 = JSON.parse(localStorage.getItem('colorSelector2Local'));
+    var storedFontSize = localStorage.getItem('fontSizeLocal');
+    var storedTitleSize = localStorage.getItem('titleSizeLocal');
     document.getElementById('11').textContent = storedTeamNames[0];
     document.getElementById('12').textContent = storedTeamScores[0];
     document.getElementById('21').textContent = storedTeamNames[1];
@@ -20,8 +25,17 @@ setInterval(function(){
     document.getElementById('61').textContent = storedTeamNames[5];
     document.getElementById('62').textContent = storedTeamScores[5];
     if (storedShowBKG == "on"){
-        console.log("YO");
+        document.getElementById("mainbody").style.backgroundColor = storedColorSelector;
     } else {
-        console.log("YO2")
+        document.getElementById("mainbody").style.backgroundColor = "white";
     }
+    if (storedShowTXT == "on"){
+        document.getElementById("mainbody").style.color = storedColorSelector2;
+    } else {
+        document.getElementById("mainbody").style.color = "black";
+    }
+    // console.log("hello " + storedFontSize)
+    // addCSS(`#scoreboard{ font-size:${JSON.stringify(storedTitleSize)}; }`);
+    // document.getElementById("showname").style.fontSize = storedTitleSize;
+    // console.log(storedTitleSize + ", " + storedFontSize);
 },3000);
